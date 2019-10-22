4231b5485937cdd6a297e5dcf109e8e97057e320
Tyler Weiss <tyler.k.weiss@gmail.com>
2019-10-22 08:54:24 -0400

    cleaned things up

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ let detroitGC = true;
 let lifeEvents = [
   "i was born in Ann Arbor, Michigan.",
   "I went to Eastern Michigan University.",
-  "I got 7th place in state bowling tournement",
+  "I got 7th place in state bowling tournement when I was 13.",
   "I am married."
 ];
 
@@ -25,14 +25,13 @@ let counter = 0;
 
 while (true) {
   let randomNumber = Math.floor(Math.random(0) * 10) + 1;
-  console.log(randomNumber);
   if (randomNumber !== 5) {
     counter++;
-    console.log(`randomNumber !== 5`);
+    console.log(`${randomNumber} !== 5`);
   } else {
     counter++;
     console.log(
-      `it took ${counter} iterations to randomly generate the number 5`
+      `${randomNumber} === 5. It took ${counter} iterations to randomly generate the number 5.`
     );
     break;
   }
